490585441c08c9de4197f62b1b9fd4be5fcbf0cc
SAKAGUCHI Takashi <hamajyotan@gmail.com>
2024-03-30 02:47:52 +0900

    first commit
    bundle gem tottoruby_rps

---
+++ lib/tottoruby_rps.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tottoruby_rps/version"
+
+module TottorubyRps
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tottoruby_rps/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module TottorubyRps
+  VERSION = "0.1.0"
+end
